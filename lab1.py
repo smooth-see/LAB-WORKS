6d01bd7f3e3e028875b5c66e27271890cc939ad3
smooth-see <sayakedige7@gmail.com>
2025-01-22 23:06:25 +0500

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,45 @@
+################################# 
+# print("Hello world")
+################################# 
+# if 5 > 2:
+#     print("5 is greater")
+################################# 
+# a = int(5)
+# b = str("Professor Saudakas Best!")
+# c = float(3.14)
+# print(a)
+# print(b)
+# print(c)
+#################################
+# lessons = ["PP1", "PP2", "PP3"]
+# a, b, c = lessons
+# print(a)
+# print(b)
+# print(c)
+#################################
+# x = 5
+# y = 4
+# print(5 + 4)
+# print(5 - 4)
+# print(5 * 4)
+# print(5 / 4)
+#################################
+# def Py_lab1():
+#     global x
+#     x = "fantastic"
+#     print("PP2 with professor Saudakas is " + x)
+# Py_lab1()
+#################################
+# a = "Hello"
+# print(a[1])
+#################################
+# for i in "hello":
+#     print(i)
+#################################
+# a = 34
+# t = f"My name is CR7, I am {a}"
+# print(t)
+#################################
+# p = 667
+# t = f"The price is {p:.2f} dollars"
+# print(t)
